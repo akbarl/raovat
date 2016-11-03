58542028833d5058e12bc0b97a4d61a2d1039334
akbarl <akbarl@live.com>
2016-11-03 22:55:07 +0700

    eightth commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Request, App\Category;
 
 class HomeController extends Controller
 {
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+		$categories = Category::all();
+        return view('home')->with('categories',$categories);
     }
 }
