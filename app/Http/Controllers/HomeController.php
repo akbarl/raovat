d676cdaf438bac7a2f247723fac19780f4b0f249
akbarl <akbarl@live.com>
2016-11-17 18:59:22 +0700

    v7 copy

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Location, App\Category;
 class HomeController extends Controller
 {
     /**
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+		$locations = Location::all();
+		$categories = Category::all();
+        return view('home')->with(['locations' => $locations, 'categories' => $categories]);
     }
 }
