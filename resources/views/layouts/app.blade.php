a7be8106087a35af3d50eee7a88d6d7b0255dfd6
akbarl <akbarl@live.com>
2016-12-02 23:29:42 +0700

    v20 commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -135,7 +135,7 @@
 
     <!-- Scripts -->
     <!-- <script src="/js/app.js"></script> -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="/js/jquery.min.js"></script>
 	<!-- <script type='text/javascript' src="/js/jquery.timeago.js"></script> -->
 	<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
 </body>
